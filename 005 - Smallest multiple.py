fc33b737ce304bc2b3868bdfe0e43fbc444c61c2
dstokes <Douglas.Stokes@tpicap.com>
2019-07-31 07:38:02 +0900

    005 - Smallest multiple passes all test cases

--- 005 - Smallest multiple.py
+++ 005 - Smallest multiple.py
@@ -9,12 +9,31 @@ Contraints:
 1 <= N <= 40
 """
 
+from functools import reduce
+from operator import mul
 import sys
 sys.stdin = open(__file__.replace('.py', ' - Inputs.txt'))  # Simulate inputs from stdin - remove this on Hackerrank
 
 
+def remove_factor(num: int, factor: int) -> int:
+    """
+    return num divided by factor if this is possible with no remainder, if not possible return num
+    """
+    if num % factor == 0:
+        return num // factor
+    else:
+        return num
+
+
 def smallest_multiple(num: int) -> int:
-    pass
+    """
+    calculate the smallest multiple of all numbers from 1 to num by creating a chain of each integer 1 <= i <= num
+    with any existing factors in the chain removed from each i, and return the product of the chain
+    """
+    chain = []
+    for i in range(1, num + 1):
+        chain.append(reduce(remove_factor, chain, i))
+    return reduce(mul, chain)
 
 
 if __name__ == '__main__':
