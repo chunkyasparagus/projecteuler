b67945a6b38804d5e53d23da7761c0dbf7f20ed4
dstokes <Douglas.Stokes@tpicap.com>
2019-07-31 12:41:45 +0900

    007 - 10001st prime fails 2 test cases, timeout

--- 007 - 10001st prime.py
+++ 007 - 10001st prime.py
@@ -7,15 +7,26 @@ What is the Nth prime number?
 Constraints:  1 <= number of test cases T <= 1000, 1 <= N <= 10000
 """
 
+from typing import List
 import sys
 sys.stdin = open(__file__.replace('.py', ' - Inputs.txt'))  # Simulate inputs from stdin - remove this on Hackerrank
 
 
-def nth_prime(num: int) -> int:
+def is_odd_prime(num: int) -> bool:
+    return num > 2 and not any(num % fac == 0 for fac in range(3, num // 2 + 1, 2))
+
+
+def nth_prime(nth: int, primes: List[int] = [None, 2, 3]) -> int:
     """
     return the nth prime number
     """
-    pass
+    #  extend the list if nec
+    for i in range(len(primes), nth + 1):
+        testnum = primes[-1] + 2
+        while not is_odd_prime(testnum):
+            testnum += 2
+        primes.append(testnum)
+    return primes[nth]
 
 
 if __name__ == '__main__':
