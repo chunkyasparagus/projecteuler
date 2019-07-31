279cb739c0c616da09c930e53a58c6ce4ea1e690
dstokes <Douglas.Stokes@tpicap.com>
2019-07-31 15:58:15 +0900

    008 - Largest product in a series passes all tests

--- 008 - Largest product in a series.py
+++ 008 - Largest product in a series.py
@@ -7,12 +7,17 @@ Example, for 10-digit number 3675356291, and selecting K=5 digits, we have 36753
 In this case, 6 * 7 * 5 * 3 * 5 gives  maximum product of 3150  (answer: 3150)
 """
 
+from operator import mul
+from functools import reduce
 import sys
 sys.stdin = open(__file__.replace('.py', ' - Inputs.txt'))  # Simulate inputs from stdin - remove this on Hackerrank
 
 
-def largest_product_of_digits(snum: str, k_digits: int) -> int:
-    pass
+def largest_product_of_digits(stringnum: str, k_digits: int) -> int:
+    return max(
+        reduce(mul, (int(digit) for digit in stringnum[start_digit:start_digit + k_digits]))
+        for start_digit in range(len(stringnum) - k_digits)
+    )
 
 
 if __name__ == '__main__':
