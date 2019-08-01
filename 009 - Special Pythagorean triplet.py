26c869c9c3cea9df85853838e7cd83e85b8a85bd
dstokes <Douglas.Stokes@tpicap.com>
2019-08-01 09:03:04 +0900

    009 - Special Pythagorean triplet fails with wrong answer :(

--- 009 - Special Pythagorean triplet.py
+++ 009 - Special Pythagorean triplet.py
@@ -7,36 +7,56 @@ Given N, check if there exists any Pythagorean triplet for which a + b + c = N
 Find maximum possible value of abc among all of the possibilities.  If there is no such possibility, print -1
 """
 
+from itertools import count, chain
 from functools import reduce
+from collections import OrderedDict
 from operator import mul
-from typing import List, Tuple
+from typing import Iterator, List
 import sys
 sys.stdin = open(__file__.replace('.py', ' - Inputs.txt'))  # Simulate inputs from stdin - remove this on Hackerrank
 
 
-def pythagorean_triplets(perimeter: int) -> List[Tuple[int, int, int]]:
+def euclids_pythagorean_triples() -> Iterator:
     """
-    gets a list of Pythagorean Triplets for a given perimeter
-    :param perimeter: sum of the sides of a triangle whose sides are the Pythagorean Triplet
-    :return: list of possible Pythagorean Triplets, or an empty list
+    Generator iterating over all of Euclids Pythagorean Triples, given by
+    a = m^2 - n^2, b = 2mn, c = m^2 - n^2 for m > n > 0, yielding all primative and some non-primative Pythagorean
+    Triples
+    :return: Iterator returning Tuple[Tuple[int, int, int], Tuple[int, int]] where 1st item is the PT, 2nd is (m, n)
     """
-    triplets = []
-    for side1 in range(1, perimeter // 2):
-        for side2 in range(side1, perimeter - side1):
-            side3 = perimeter - side1 - side2
-            if side1 ** 2 + side2 ** 2 == side3 ** 2:
-                triplets.append((side1, side2, side3))
-    return triplets
+    for m in count(2):
+        for n in range (1, m):
+            a, b, c = m ** 2 - n ** 2, 2 * m * n, m ** 2 + n ** 2
+            yield ((a, b, c), (m, n))
 
 
-def get_maximum_abc(triplets: List[Tuple[int, int, int]]) -> int:
+# noinspection PyDefaultArgument
+def pythagorean_triples_given_n(n: int, saved_triples=OrderedDict(), ept=euclids_pythagorean_triples()) -> List:
     """
-    out of a list of Pythagorean Triplets (PT), get the maximum a * b * c, where a, b, c are members of a PT
+    get a list of all Pythagorean Triples (a, b, c) such that a + b + c == n
+    :param n:
+    :param saved_triples: saved triples from euclids_pythagorean_triples to avoid cost of regenerating
+    :param ept: Iterator here to avoid generating from start of set again
+    :return: List of Tuple[int, Tuple[int, int, int]] where the first member is the product a * b * c,
+    second item is the Pythagorean Triple
     """
-    if triplets:
-        return max(reduce(mul, triplet) for triplet in triplets)
-    else:
-        return -1
+    n_triples = set()
+
+    for triple in chain(saved_triples, ept):
+        saved_triples[triple] = None  # save this triple to avoid regeneration later
+
+        if 2 * triple[1][0] ** 2 + 2 * triple[1][0] * triple[1][1] > n:
+            break  # there can be no more triples where a + b + c == n
+
+        total = sum(triple[0])
+        factor, modulo = divmod(n, total)
+        return_triple = tuple(sorted([item * factor for item in triple[0]]))
+        product = reduce(mul, return_triple)
+        if modulo == 0:  # this is a valid triple for a + b + c == n
+            n_triples.add(
+                (product, return_triple)
+            )
+
+    return sorted(n_triples, key=lambda item: -item[0])  # sorting to give highest product first
 
 
 if __name__ == '__main__':
@@ -45,4 +65,8 @@ if __name__ == '__main__':
     for a0 in range(t):
         n = int(input().strip())
         #  the following is my own
-        print(get_maximum_abc(pythagorean_triplets(n)))
+        triples = pythagorean_triples_given_n(n)
+        if triples:
+            print(triples[0][0])
+        else:
+            print(-1)
