81663a363acd8270e8ecb536be2a08b491c76f41
chunkyasparagus <icanseethepubfromhere@gmail.com>
2019-08-06 23:00:28 +0900

    fix small bug on 011 (used const val not argument), added comments

--- 011 - Largest product in a grid.py
+++ 011 - Largest product in a grid.py
@@ -3,7 +3,7 @@
 What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, diagonal) in a
 20x20 grid of integers?
 
-Input consists of 20 lines of 20 integers i,  where 0 <= i <= 100
+Input consists of 20 lines of 20 integers i, separated by a space each,  where 0 <= i <= 100
 
 """
 
@@ -20,22 +20,22 @@ def greatest_product(fullgrid: List[List[int]], conseq_len=4) -> int:
     appearing in a two-dimensional matrix (list of lists) fullgrid.  the grid can be any size.
     """
     def not_in_right_gutter(test_i):
-        return 0 <= test_i % grd_wdth <= grd_wdth - conseq_len
+        return 0 <= test_i % grd_wdth <= grd_wdth - conseq_len  # index is at least conseq_len from rhs of grid
 
     def not_in_left_gutter(test_i):
-        return conseq_len <= test_i % grd_wdth <= grd_wdth - 1
+        return conseq_len <= test_i % grd_wdth <= grd_wdth - 1  # index is at least conseq_len from lhs of grid
 
     def not_in_bottom_gutter(test_i):
-        return test_i < grd_hght * grd_wdth - grd_wdth * conseq_len
+        return test_i < grd_hght * grd_wdth - grd_wdth * conseq_len  # index is at least conseq_len from bottom
 
     grd_hght, grd_wdth = len(fullgrid), len(fullgrid[0])
-    flat_grid = [item for row in fullgrid for item in row]
+    flat_grid = [item for row in fullgrid for item in row]  # create single flat list for slicing
 
     all_sets = []
     for i in range(len(flat_grid)):
         if not_in_right_gutter(i):
             # append horizontal line starting from this position
-            all_sets.append(flat_grid[i: i + 4])
+            all_sets.append(flat_grid[i: i + conseq_len])
         if not_in_bottom_gutter(i):
             # append vertical line starting from this position
             all_sets.append(flat_grid[i: i + grd_wdth * conseq_len: grd_wdth])
